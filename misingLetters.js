9c8ddeed20e2ca96a51ec6c2b4f5c8d6a714de9f
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-21 19:24:32 -0400

    have psuedocode for algorithim

--- misingLetters.js
+++ misingLetters.js
@@ -1,8 +1,16 @@
 // Find the missing letter in the passed letter range and return it.
 // If all letters are present in the range, return undefined.
 
+// convert all characters in str to their unicode values
+// if the range between values is 26 than we can return undefined
+// loop through the values
+// if the next value is not +1 the previous value
+// we return that value converted back to an alpha character
+
 function fearNotLetter(str) {
   return str;
 }
 
-fearNotLetter("abce");
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("aabcdefghjklmno"));
+console.log(fearNotLetter("stvwx"));
