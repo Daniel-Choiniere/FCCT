22c702431bc00e65c48aecff3300279a0d7dc7d9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-22 16:52:03 -0400

    passing all test cases with only one source key, if two we fail

--- whatIsInAName.js
+++ whatIsInAName.js
@@ -5,18 +5,6 @@
 // and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument),
 // because it contains the name and its value, that was passed on as the second argument.
 
-// function whatIsInAName(collection, source) {
-//   var answer = [];
-//   collection.forEach(function(element) {
-//     // console.log(element.last);
-//     // console.log(source.last);
-//     if (element.last == source.last) {
-//       answer.push(element);
-//     }
-//   });
-//   return answer;
-// }
-
 function whatIsInAName(collection, source) {
   var answer = [];
 
@@ -24,12 +12,30 @@ function whatIsInAName(collection, source) {
     // console.log(collection[i]);
     var collectionKeys = Object.keys(collection[i]);
     var sourceKeys = Object.keys(source);
+
+    var collectionValues = Object.values(collection[i]);
+    var sourceValues = Object.values(source);
+
     // console.log("Collection keys", collectionKeys);
     // console.log("Source keys", sourceKeys);
+    // console.log("Collection values", collectionValues);
+    // console.log("Source values", sourceValues);
+
+    // if (collection[i].indexOf(sourceValues[i])) {
+    // console.log(collection[i]);``
+    // console.log(sourceValues[i]);
+
+    if (Object.values(collection[i]).indexOf(sourceValues[0]) > -1) {
+      console.log(collection[i]);
+    }
   }
-  return answer;
+  // return answer;
 }
 
+// var obj = { a: "test1", b: "test2" };
+// if (Object.values(obj).indexOf("test1") > -1) {
+//   console.log("has test1");
+// }
 //
 //
 //
@@ -38,44 +44,40 @@ function whatIsInAName(collection, source) {
 //
 //
 //
-var test1 = whatIsInAName(
-  [
-    { first: "Romeo", last: "Montague" },
-    { first: "Mercutio", last: null },
-    { first: "Tybalt", last: "Capulet" }
-  ],
-  { last: "Capulet" }
-);
+// var test1 = whatIsInAName(
+//   [
+//     { first: "Romeo", last: "Montague" },
+//     { first: "Mercutio", last: null },
+//     { first: "Tybalt", last: "Capulet" }
+//   ],
+//   { last: "Capulet" }
+// );
 
-var test2 = whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {
-  apple: 1
-});
+// var test2 = whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {
+//   apple: 1
+// });
 
-var test3 = whatIsInAName(
-  [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
-  { apple: 1, bat: 2 }
-);
+// var test3 = whatIsInAName(
+//   [{ apple: 1, bat: 2 }, { bat: 2 }, { apple: 1, bat: 2, cookie: 2 }],
+//   { apple: 1, bat: 2 }
+// );
 
-var test4 = whatIsInAName(
-  [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
-  { apple: 1, cookie: 2 }
-);
+// DOES NOT PASS - NEED TO HANDLE CHECKING BOTH THE SOURCE KEYS NOT JUST ONE
+// var test4 = whatIsInAName(
+//   [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+//   { apple: 1, cookie: 2 }
+// );
 
-var test5 = whatIsInAName(
-  [
-    { apple: 1, bat: 2 },
-    { apple: 1 },
-    { apple: 1, bat: 2, cookie: 2 },
-    { bat: 2 }
-  ],
-  { apple: 1, bat: 2 }
-);
+// DOES NOT PASS
+// var test5 = whatIsInAName(
+//   [
+//     { apple: 1, bat: 2 },
+//     { apple: 1 },
+//     { apple: 1, bat: 2, cookie: 2 },
+//     { bat: 2 }
+//   ],
+//   { apple: 1, bat: 2 }
+// );
 
-var test6 = whatIsInAName([{ a: 1, b: 2, c: 3 }], { a: 1, b: 9999, c: 3 });
-
-console.log("Test ONE: " + test1);
-console.log("Test TWO: " + test2);
-console.log("Test THREE: " + test3);
-console.log("Test FOUR: " + test4);
-console.log("Test FIVE: " + test5);
-console.log("Test SIX: " + test6);
+// DOES NOT PASS
+// var test6 = whatIsInAName([{ a: 1, b: 2, c: 3 }], { a: 1, b: 9999, c: 3 });
