34d7eb382961818cdc6672d114406ba6e2aa0f65
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-21 20:12:02 -0400

    logic working for shouder case, strings originally matching

--- misingLetters.js
+++ misingLetters.js
@@ -7,10 +7,30 @@
 // if the next value is not +1 the previous value
 // we return that value converted back to an alpha character
 
+// function fearNotLetter(str) {
+//   var arr = [];
+//   for (var i = 0; i < str.length; i++) {
+//     arr.push(str[i].charCodeAt());
+//   }
+
+//   for (var i = 1; i < arr.length; i++) {
+//     if (arr[0] + 1 == arr[1]) {
+//       return String.fromCharCode(arr[0] + 1);
+//     }
+//   }
+
+//   return arr;
+// }
+
 function fearNotLetter(str) {
-  return str;
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  if (str == alphabet) {
+    return undefined;
+  }
+  return "Not undefined";
 }
 
 console.log(fearNotLetter("abce"));
 console.log(fearNotLetter("aabcdefghjklmno"));
 console.log(fearNotLetter("stvwx"));
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
